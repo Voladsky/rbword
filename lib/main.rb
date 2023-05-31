168101a9dd550d28e3b03a206f2e8d8a57944b34
Владислав Крылов <krylov@sfedu.ru>
2023-05-31 15:15:44 +0300

    Delete main.rb

--- lib/main.rb
+++
@@ -1,8 +0,0 @@
-def read_folder(folder_path)
-  files = Dir.entries(folder_path).select { |file| File.file? folder_path + "/" + file}
-  files.select! { |file| file.end_with? ".txt" }
-  files.map! { |file| folder_path + "/" + file }
-  puts files
-end
-
-read_folder("../test/test_folder")
